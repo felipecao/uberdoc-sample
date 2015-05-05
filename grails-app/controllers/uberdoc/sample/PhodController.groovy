34d1d753d0dee6b7486b59827c44cfd050533289
felipe <felipe@uberall.com>
2015-05-05 17:12:11 +0200

    improved example a bit

--- grails-app/controllers/uberdoc/sample/PhodController.groovy
+++ grails-app/controllers/uberdoc/sample/PhodController.groovy
@@ -1,45 +1,64 @@
 package uberdoc.sample
 
-import com.uberall.uberdoc.annotation.Error
-import com.uberall.uberdoc.annotation.Errors
-import com.uberall.uberdoc.annotation.HeaderParam
-import com.uberall.uberdoc.annotation.HeaderParams
-import com.uberall.uberdoc.annotation.QueryParam
-import com.uberall.uberdoc.annotation.Resource
+import com.uberall.uberdoc.annotation.UberDocError
+import com.uberall.uberdoc.annotation.UberDocErrors
+import com.uberall.uberdoc.annotation.UberDocHeader
+import com.uberall.uberdoc.annotation.UberDocHeaders
+import com.uberall.uberdoc.annotation.UberDocQueryParam
+import com.uberall.uberdoc.annotation.UberDocQueryParams
+import com.uberall.uberdoc.annotation.UberDocResource
+import com.uberall.uberdoc.annotation.UberDocUriParam
+import com.uberall.uberdoc.annotation.UberDocUriParams
 import sample.Pod
 
 import static org.springframework.http.HttpStatus.*
 import grails.transaction.Transactional
 
-@Errors([
-        @Error(errorCode = "XYZ123", httpCode = 412, description = "this is a general 412 error, to be applied to all actions/resources in this file"),
-        @Error(errorCode = "ABC456", httpCode = 400, description = "this is a general 409 error, to be applied to all actions/resources in this file"),
-        @Error(errorCode = "CONF409", httpCode = 409, description = "all actions in this file may throw a 409 to indicate conflict")
+@UberDocErrors([
+        @UberDocError(errorCode = "XYZ123", httpCode = 412, description = "this is a general 412 error, to be applied to all actions/resources in this file"),
+        @UberDocError(errorCode = "ABC456", httpCode = 400, description = "this is a general 409 error, to be applied to all actions/resources in this file"),
+        @UberDocError(errorCode = "CONF409", httpCode = 409, description = "all actions in this file may throw a 409 to indicate conflict")
 ])
-@HeaderParams([
-        @HeaderParam(name = "publicToken", sampleValue = "all methods in this file should send a header param", description = "this param should be sent within the headers"),
-        @HeaderParam(name = "other token", sampleValue = "just some other token that every method should send", description = "this param should also be sent within the headers")
+@UberDocHeaders([
+        @UberDocHeader(name = "publicToken", sampleValue = "all methods in this file should send a header param", description = "this param should be sent within the headers"),
+        @UberDocHeader(name = "other token", sampleValue = "just some other token that every method should send", description = "this param should also be sent within the headers")
 ])
 @Transactional(readOnly = true)
 class PhodController {
 
     static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    @Resource(responseObject = Pod, description = "this resource allows all Pods to be retrieved from DB")
+    @UberDocResource(responseObject = Pod, description = "this resource allows all Pods to be retrieved from DB")
+    @UberDocQueryParams([
+            @UberDocQueryParam(name = "offset", required = false, description = "offset used for pagination"),
+            @UberDocQueryParam(name = "locationIds", required = false, description = "collection of ids to be retrieved", isCollection = true)
+    ])
+    @UberDocQueryParam(name = "max", required = true, description = "Maximum number of records to display")
     def list(Integer max) {
         params.max = Math.min(max ?: 10, 100)
         respond Phod.list(params), model:[phodInstanceCount: Phod.count()]
     }
 
-    @Resource(requestObject = Pod, responseCollectionOf = Pod, description = "this resource allows all Pods to be retrieved from DB")
-    @Error(errorCode = "NF404", httpCode = 404, description = "returned if the resource does not exist")
-    @QueryParam(name = "id", description = "the id of the Pod to be retrieved from DB", sampleValue = "4")
+    @UberDocResource(requestObject = Pod, responseCollectionOf = Pod, description = "this resource allows all Pods to be retrieved from DB")
+    @UberDocError(errorCode = "NF404", httpCode = 404, description = "returned if the resource does not exist")
+    @UberDocUriParam(name = "other id", description = "the other id of the Pod to be retrieved from DB", sampleValue = "4")
     def get(Phod phodInstance) {
         respond phodInstance
     }
 
-    @Resource(object = Pod, description = "this resource creates Pods")
-    @HeaderParam(name = "some header param", sampleValue = "hdr", description = "this is just something else to be sent on creation")
+    @UberDocResource(object = Pod, description = "this resource creates Pods")
+    @UberDocHeader(name = "some header param", sampleValue = "hdr", description = "this is just something else to be sent on creation")
+    @UberDocHeaders([
+            @UberDocHeader(name = "a token", sampleValue = "just a sample token", description = "param used just for testing purposes")
+    ])
+    @UberDocErrors([
+            @UberDocError(errorCode = "XYZ666", httpCode = 666, description = "666 - the beast has attacked this controller")
+    ])
+    @UberDocError(errorCode = "XYZ999", httpCode = 999, description = "999 - the reverse beast has attacked this controller")
+    @UberDocUriParams([
+            @UberDocUriParam(name = "firstId", description = "the first id", sampleValue = "4"),
+            @UberDocUriParam(name = "secondId", description = "the second id", sampleValue = "4")
+    ])
     @Transactional
     def create(Phod phodInstance) {
         if (phodInstance == null) {
@@ -63,8 +82,8 @@ class PhodController {
         }
     }
 
-    @Resource(object = Pod)
-    @QueryParam(name = "id", description = "the id of the Pod to be retrieved from DB", sampleValue = "4")
+    @UberDocResource(object = Pod)
+    @UberDocQueryParam(name = "id", description = "the id of the Pod to be retrieved from DB", sampleValue = "4")
     @Transactional
     def update(Phod phodInstance) {
         if (phodInstance == null) {
@@ -88,7 +107,7 @@ class PhodController {
         }
     }
 
-    @QueryParam(name = "id", description = "the id of the Pod to be retrieved from DB", sampleValue = "4")
+    @UberDocQueryParam(name = "id", description = "the id of the Pod to be retrieved from DB", sampleValue = "4")
     @Transactional
     def delete(Phod phodInstance) {
 
