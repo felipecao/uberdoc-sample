35f867aa9ccf85734e64fcd643761c4e0c29bff5
Florian Langenhahn <florian.langenhahn@uberall.com>
2015-05-06 11:17:21 +0200

    added annotations to Pessoa domain class

--- grails-app/domain/uberdoc/sample/Pessoa.groovy
+++ grails-app/domain/uberdoc/sample/Pessoa.groovy
@@ -1,9 +1,16 @@
 package uberdoc.sample
 
+import com.uberall.uberdoc.annotation.UberDocModel
+import com.uberall.uberdoc.annotation.UberDocProperty
+
+@UberDocModel(description = "this is a pessoa")
 class Pessoa {
 
+    @UberDocProperty(description = "the first name", sampleValue = "Manfred")
     String firstName
+    @UberDocProperty(description = "the last name", sampleValue = "Schnösebrink")
     String lastName
+    @UberDocProperty(description = "a list of nick names", sampleValue = "[manne, fredbert, heribert]")
     List<String> nickNames
 
     static transients = ['fullName']
